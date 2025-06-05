63fbef4e5638cd2a41d041b5acdc808117d8f900
bender2352 <165169475+bender2352@users.noreply.github.com>
2025-06-05 17:37:11 +0200

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,12 +1,12 @@
 resource "null_resource" "this" {
-  count = var.count
+  null_resource_count = var.null_resource_count
 
   provisioner "local-exec" {
     command = "echo 'This is a placeholder resource. No real infrastructure is provisioned.'"
   }
 }
 
-variable "count" {
+variable "null_resource_count" {
   type    = number
   default = 0
 }
