3e45246ba75525f70e2abef62c1d936bd3649b59
bender2352 <165169475+bender2352@users.noreply.github.com>
2025-06-05 17:50:11 +0200

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,6 @@
 module "dev" {
   source = "../modules"
 
-  # Change 0 to 1 to and open a pull request to trigger Terrateam
-  null_resource_count = 0
+  # Change 0 to 1 and open a pull request to trigger Terrateam
+  null_resource_count = 1
 }
